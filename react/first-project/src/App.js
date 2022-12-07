b85f7d0a07fce722cea2c8a8aa8680e9d77ea4fa
Mutasem Siam <mutasemsiam4@gmail.com>
2022-12-07 22:04:00 +0200

    initial

---
+++ react/first-project/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+// import React from 'react';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello Dojo!</h1>
+    <p>Things I need to do:</p>
+    <ul className='mylist'>
+      <li>Learn React</li>
+      <li>Climb</li>
+      <li>Walk</li>
+      <li>Run</li>
+    </ul>
+   
+    </div>
+  );
+}
+
+export default App;
