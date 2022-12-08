d5efa5caf643f5907f0ee36503f5e7ce1f477605
Mutasem Siam <mutasemsiam4@gmail.com>
2022-12-08 14:32:39 +0200

    some edits

--- react/prop-it-up/src/App.js
+++ react/prop-it-up/src/App.js
@@ -7,7 +7,7 @@ function App() {
    <PersonCard firstName={"Mutasem"} lastName={"Siam"} age={20} hairColor={"Black"} />
    <PersonCard firstName={"Ali"} lastName={"Mohammad"} age={23} hairColor={"Black"} />
    <PersonCard firstName={"Mohammad"} lastName={"Siam"} age={15} hairColor={"Black"} />
-   <PersonCard firstName={"Ahmad"} lastName={"Siam"} age={2} hairColor={"Black"} />
+   <PersonCard firstName={"Ahmad"} lastName={"Siam"} age={2} hairColor={"Black"}> </PersonCard>
    </div>
   );
 }
