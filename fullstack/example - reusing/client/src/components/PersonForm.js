be0fcd3d2024d218177fa5bf0593f5358cb0e0f1
Mutasem Siam <mutasemsiam4@gmail.com>
2022-12-23 19:32:50 +0200

    adding Material UI

--- fullstack/example - reusing/client/src/components/PersonForm.js
+++ fullstack/example - reusing/client/src/components/PersonForm.js
@@ -1,4 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react';
+import {
+    Paper,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    Button
+} from '@material-ui/core';
 export default props => {
     const { initialFirstName, initialLastName, onSubmitProp } = props;
     const [firstName, setFirstName] = useState(initialFirstName);
@@ -10,24 +17,36 @@ export default props => {
     }
 
     return (
-        <form onSubmit={onSubmitHandler}>
-            <p>
-                <label>First Name</label><br />
-                <input
-                    type="text"
-                    name="firstName" value={firstName}
-                    onChange={(e) => { setFirstName(e.target.value) }} />
-            </p>
-            <p>
-                <label>Last Name</label><br />
-                <input
-                    type="text"
-                    name="lastName"
-                    value={lastName}
-                    onChange={(e) => { setLastName(e.target.value) }} />
-            </p>
-            <input type="submit" />
-        </form>
+        <>
+            <Paper elevation={3}>
+                <h1>Welcome to the form</h1>
+            </Paper>
+            <Paper elevation={3}>
+                <form onSubmit={onSubmitHandler}>
+                    <p>
+                        <label>First Name</label><br />
+                        <input
+                            type="text"
+                            name="firstName" value={firstName}
+                            onChange={(e) => { setFirstName(e.target.value) }} />
+                    </p>
+                    <p>
+                        <label>Last Name</label><br />
+                        <input
+                            type="text"
+                            name="lastName"
+                            value={lastName}
+                            onChange={(e) => { setLastName(e.target.value) }} />
+                    </p>
+                 
+
+                    {/* <input type="submit" /> */}
+                    <Button type="submit" variant="contained" color="primary">
+                        Submit
+                    </Button>
+                </form>
+            </Paper>
+        </>
     )
 }
 
